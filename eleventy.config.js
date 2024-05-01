cae595710fd881faa9c43f62aef2a9d7feb237c8
twhite96 <github-action@actions-template-sync.noreply.github.com>
2024-05-01 00:16:57 +0000

    chore(template): merge template changes :up:
    Signed-off-by: twhite96 <github-action@actions-template-sync.noreply.github.com>

--- eleventy.config.js
+++ eleventy.config.js
@@ -21,22 +21,11 @@ const {
   stripHtml,
   minifyCss,
   minifyJs,
-  splitlines,
-  getWebmentionsForUrl,
-  webmentionSize,
-  webmentionsByType,
-  isOwnWebmention,
-  sortWebmentions,
-  shuffle
+  splitlines
 } = require('./config/filters/index.js');
 
-const {
-  Icon,
-  Callout
-} = require('./config/utils/components.js')
-
 // module import shortcodes
-const {imageShortcode, includeRaw, liteYoutube, asideShortcode, breakoutShortcode, insertionShortcode} = require('./config/shortcodes/index.js');
+const {imageShortcode, includeRaw, liteYoutube} = require('./config/shortcodes/index.js');
 
 // module import collections
 const {getAllPosts} = require('./config/collections/index.js');
@@ -73,7 +62,6 @@ module.exports = eleventyConfig => {
 
   // 	---------------------  Custom filters -----------------------
   eleventyConfig.addFilter('toIsoString', toISOString);
-  eleventyConfig.addFilter('shuffle', shuffle);
   eleventyConfig.addFilter('formatDate', formatDate);
   eleventyConfig.addFilter('toAbsoluteUrl', toAbsoluteUrl);
   eleventyConfig.addFilter('stripHtml', stripHtml);
@@ -86,11 +74,6 @@ module.exports = eleventyConfig => {
   eleventyConfig.addFilter('toJson', JSON.stringify);
   eleventyConfig.addFilter('fromJson', JSON.parse);
 
-  eleventyConfig.addFilter('getWebmentionsForUrl', getWebmentionsForUrl);
-  eleventyConfig.addFilter('webmentionSize', webmentionSize);
-  eleventyConfig.addFilter('webmentionsByType', webmentionsByType);
-  eleventyConfig.addFilter('isOwnWebmention', isOwnWebmention);
-  eleventyConfig.addFilter('sortWebmentions', sortWebmentions);
   eleventyConfig.addFilter('keys', Object.keys);
   eleventyConfig.addFilter('values', Object.values);
   eleventyConfig.addFilter('entries', Object.entries);
@@ -98,12 +81,6 @@ module.exports = eleventyConfig => {
   // 	--------------------- Custom shortcodes ---------------------
   eleventyConfig.addNunjucksAsyncShortcode('eleventyImage', imageShortcode);
   eleventyConfig.addShortcode('youtube', liteYoutube);
-
-  eleventyConfig.addPairedShortcode('callout', Callout)
-  eleventyConfig.addShortcode('icon', Icon)
-  eleventyConfig.addShortcode('aside', asideShortcode);
-  eleventyConfig.addShortcode('breakout', breakoutShortcode);
-  eleventyConfig.addShortcode('insertion', insertionShortcode);
   eleventyConfig.addShortcode('include_raw', includeRaw);
   eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`); // current year, by stephanie eckles
 
@@ -138,7 +115,7 @@ module.exports = eleventyConfig => {
 
   // 	--------------------- Passthrough File Copy -----------------------
   // same path
-  ['src/assets/fonts/', 'src/assets/images/blog', 'src/assets/images/screenshots', 'src/assets/images/quick-reviews', 'src/assets/images/gallery', 'src/assets/images/template', 'src/assets/og-images'].forEach(
+  ['src/assets/fonts/', 'src/assets/images/template', 'src/assets/og-images'].forEach(
     path => eleventyConfig.addPassthroughCopy(path)
   );
 
