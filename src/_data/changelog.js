a6e1a6dbe0ff80c78ce5cafdf187aad0d853c080
tiff <github@0x8c.org>
2024-07-19 06:43:13 -0400

    Update something

--- src/_data/changelog.js
+++ src/_data/changelog.js
@@ -1,6 +1,16 @@
 module.exports = {
   log: [
     {
+      date: "2024-07-19",
+      type: "chore",
+      content: "Upgraded and downgraded Eleventy-excellent as it broke the whole damn site."
+    },
+    {
+      date: "2024-07-10",
+      type: "content",
+      content: "Updated book collection and added two new posts."
+    },
+    {
       date: "2024-07-08",
       type: "feature",
       content: "Finally incorporated the Changelog page."
